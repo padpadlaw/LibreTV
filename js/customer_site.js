44e00893690ce909afc8a4e8429218359885de39
padpadlaw <padpadlaw@gmail.com>
2026-06-26 23:18:46 +0800

    Update customer_site.js

--- js/customer_site.js
+++ js/customer_site.js
@@ -1,7 +1,10 @@
 const CUSTOMER_SITES = {
     qiqi: {
         api: 'https://www.mdzyapi.com/api.php/provide/vod',
-        name: '魔都资源',api: 'https://www.xxibaozyw.com/api.php/provide/vod',
+        name: '魔都资源',
+    }
+    qiqi: {
+        api: 'https://www.xxibaozyw.com/api.php/provide/vod',
         name: '细胞采集',
     }
 };
