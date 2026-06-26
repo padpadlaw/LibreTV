85313970780ded3f9ba5968372852ad9f6623aa6
padpadlaw <padpadlaw@gmail.com>
2026-06-26 23:51:30 +0800

    Update customer_site.js

--- js/customer_site.js
+++ js/customer_site.js
@@ -3,14 +3,6 @@ const CUSTOMER_SITES ={
         api: 'https://www.qiqidys.com/api.php/provide/vod',
         name: '七七资源',
     }
-    qiqi: {
-        api: 'https://www.qiqidys.com/api.php/provide/vod',
-        name: '七资源',
-    }
-        qiqi: {
-        api: 'https://www.qiqidys.com/api.php/provide/vod',
-        name: '七七源',
-    }
 };
 
 // 调用全局方法合并
